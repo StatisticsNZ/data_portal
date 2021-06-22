252f80546a1bcf2dc49f3f5e9318efeb483e7d44
Maya Gorbatovsky <maya.gorbatovsky@stats.govt.nz>
2021-06-22 16:24:39 +1200

    changed 'dev' to 'updates' in script calls

--- scripts/updates/__daily.R
+++ scripts/updates/__daily.R
@@ -1,14 +1,14 @@
 # Number of cases (other countries)
-source("scripts/dev/johnHopkins_globalCases.R")
+source("scripts/updates/johnHopkins_globalCases.R")
 
 # Electricity grid demand +++ Electricity grid demand by region
-source("scripts/dev/EMA_grid.R")
+source("scripts/updates/EMA_grid.R")
 
 # Broadband usage
-source("scripts/dev/chorus_daily.R")
+source("scripts/updates/chorus_daily.R")
 
 # Daily financial: Trade weighted index +++ Bank bill yields +++ Foreign exchange
-source('scripts/dev/daily_financial.R')
+source('scripts/updates/daily_financial.R')
 
 
 
